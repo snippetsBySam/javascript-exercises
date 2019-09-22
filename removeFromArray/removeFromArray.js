826f3926751ad3640128cac2e6ce1c9461006975
Sam <snippetsbysam@outlook.com>
2019-09-23 00:29:25 +0100

    removeFromArray solved

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalArray, ...stringsToRemove) {
+    let removedArray = originalArray;
+    let valuesToRemove = Array.from(stringsToRemove);
+    for (let i = 0; i < valuesToRemove.length; i++) {
+        if (removedArray.indexOf(valuesToRemove[i]) == -1) {
+            continue;
+        }
+        removedArray.splice(removedArray.indexOf(valuesToRemove[i]), 1);
+    }
+    return removedArray;
 }
 
 module.exports = removeFromArray
