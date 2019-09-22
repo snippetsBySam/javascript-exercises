2855014327027d82a621c87481dc2f5bf87ddefc
Sam <snippetsbysam@outlook.com>
2019-09-22 20:31:07 +0100

    repeatString solved

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+const repeatString = function(stringToRepeat, timesToRepeat) {
+    let repeatedString = '';
+    if (timesToRepeat < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < timesToRepeat; i++) {
+        repeatedString += stringToRepeat;
+    }
+    return repeatedString;
 
 }
 
