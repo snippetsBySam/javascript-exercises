9d05972a855034db3eff21e7d508f3c4e172e5f6
Sam <snippetsbysam@outlook.com>
2019-09-25 00:16:47 +0100

    sumAll solved

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
+        return 'ERROR'
+    }
+    let cumulativeSum = 0;
+    let smallerNum = Math.min(num1, num2);
+    let largerNum = Math.max(num1, num2);
+    for (let i = smallerNum; i <= largerNum; i++) {
+        cumulativeSum += i;
+    }
+    return cumulativeSum;
 }
 
 module.exports = sumAll
