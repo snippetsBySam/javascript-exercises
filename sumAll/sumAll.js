f2d8330f7d0f25c1aad3c7dae77bba00deeb2046
Sam <snippetsbysam@outlook.com>
2019-09-25 00:20:57 +0100

    add semi-colon

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,6 +1,6 @@
 const sumAll = function(num1, num2) {
     if (!Number.isInteger(num1) || !Number.isInteger(num2) || num1 < 0 || num2 < 0) {
-        return 'ERROR'
+        return 'ERROR';
     }
     let cumulativeSum = 0;
     let smallerNum = Math.min(num1, num2);
