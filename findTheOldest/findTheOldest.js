ab132e905119003078ce63e1d1f8dffa1937fcb2
Sam <snippetsbysam@outlook.com>
2021-03-22 03:14:07 +0000

    solve findtheoldest

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    return oldestPerson = people.reduce((oldest, person) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const personAge = getAge(person.yearOfBirth, person.yearOfDeath);
+        return oldestAge > personAge ? oldest : person;
+    })
 
+}
+
+const getAge = function(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
 }
 
 module.exports = findTheOldest
