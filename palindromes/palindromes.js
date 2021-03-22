a8e53eb768c47800273006f42f2379d611f7f25f
Sam <snippetsbysam@outlook.com>
2021-03-22 00:14:51 +0000

    solve palindromes

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,4 +1,7 @@
-const palindromes = function() {
+const palindromes = function(string) {
+    filteredString = string.toLowerCase().replace(/[^A-Za-z]/g, "");
+    reversedString = filteredString.split("").reverse().join("");
+    return filteredString === reversedString;
 
 }
 
